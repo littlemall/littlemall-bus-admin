5a23424d97a6149deeb7208b50b18249d730256c
wurh <ro87630872@126.com>
2019-03-13 00:27:07 -0700

    【UPD】调整系统界面
    更新调整

--- src/view/login/login.vue
+++ src/view/login/login.vue
@@ -5,7 +5,7 @@
 <template>
   <div class="login">
     <div class="login-con">
-      <Card icon="log-in" title="欢迎登录" :bordered="false">
+      <Card icon="log-in" title="欢迎登录 littmall 商城" :bordered="false">
         <div class="form-con">
           <login-form @on-success-valid="handleSubmit"></login-form>
           <p class="login-tip">输入任意用户名和密码即可</p>
@@ -29,11 +29,14 @@ export default {
     ]),
     handleSubmit ({ userName, password }) {
       this.handleLogin({ userName, password }).then(res => {
-        this.getUserInfo().then(res => {
-          this.$router.push({
-            name: this.$config.homeName
-          })
+        this.$router.push({
+          name: this.$config.homeName
         })
+        // this.getUserInfo().then(res => {
+        //   this.$router.push({
+        //     name: this.$config.homeName
+        //   })
+        // })
       })
     }
   }
