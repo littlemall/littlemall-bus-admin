528b79a87e4dbf54f6b28b9f40d628c93e09b754
wurh <ro87630872@126.com>
2019-03-13 20:06:58 -0700

    【DEL】删除项目，重新选型
    删除项目，重新选型

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/vue3-admin/' : '/',
-  pwa: {
-    name: 'vue3-admin'
-  },
-  devServer: {
-    port: 8088, // 端口号
-  }
-}
