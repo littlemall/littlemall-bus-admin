7ef6e3a02776c48a4b3961dc1c04d3fee34c64c0
wurh <ro87630872@126.com>
2020-08-25 15:21:12 +0800

    【UPD】 update deployment
     update deployment

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
+sudo docker rm -f littlemalladmin
+sudo docker rmi -f littlemall/littlemalladmin
 sudo docker build -t littlemall/littlemalladmin .
-sudo docker run --name littlemalladmincontainer -p 8088:8088 -d littlemall/littlemalladmin
+sudo docker run --name littlemalladminfe -p 8088:8088 -d littlemall/littlemalladmin
